c991f7984a41cbde5c5089f278668dc846aee5c1
Dawid Adach <d.adach@mdbootstrap.com>
2020-04-14 00:58:41 +0200

    Initial commit

---
+++ final/js/scripts.js
@@ -0,0 +1,19 @@
+function addTask() {
+    let task = document.getElementById('task').value;
+
+    if (!task)
+    return;
+
+    const text = "<li class='task'>" + task + "</li>"
+    document.getElementById('list').insertAdjacentHTML('beforeend', text)
+    document.getElementById('task').value = '';
+}
+
+document.getElementById('add').addEventListener('click', addTask);
+document.getElementById('list').addEventListener('click', function(event){
+    const element = event.target;
+    console.log(element);
+});
+
+
+
